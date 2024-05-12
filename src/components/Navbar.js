5c5a527e33486518bbe9bb16429c689f095d8629
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-12 20:01:22 +0430

    Adding NavLink to the logo brand.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+//We can also use Link instead of NavLink but it doesn't give a active class on the clicked links.
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poki' Times</a>
+        <NavLink className="brand-logo">Poki' Times</NavLink>
         <ul className="right">
           <li>
             <NavLink to="/">Home</NavLink>
