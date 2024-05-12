cb3713f459bb447d57aa8a9fd3625794e3192ea5
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-12 19:59:07 +0430

    Routes added to the project.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
-    return(
-      <div>
-        <Navbar />
-      </div>
-    )
+    return (
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from "react";
+import {Link, NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="nav-wrapper red darken-3">
-            <div className="container">
-                <a className="brand-logo">Poki' Times</a>
-                <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/About">About</a></li>
-                    <li><a href="/Contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Poki' Times</a>
+        <ul className="right">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
