f1480b62389e0db61b5525a5a65f84b5450066dc
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-14 18:59:18 +0430

    Installing axios and using API.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-        consequuntur nobis tempore libero assumenda reprehenderit ipsam beatae
-        vel, sit aliquid nemo consectetur? Quaerat deserunt, nam culpa
-        exercitationem repudiandae incidunt laudantium?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: [ ]
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(res => {
+      this.setState({
+        posts: res.data.slice(0,10)
+      })
+    })
+  }
+
+  render() {
+    const {posts} = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="card post" key={post.id}>
+            <div className="card-content">
+              <div className="card-title">{post.title}</div>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    )
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
